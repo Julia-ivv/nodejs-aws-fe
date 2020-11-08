754edaa2cd1f38e7615e05bae71f8c4cf2106dd3
Julia Ivanova <julia_sji85@mail.ru>
2020-11-08 23:29:06 +0300

    feat: implement task 4

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,9 +1,8 @@
 
 const API_PATHS = {
-  product: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
-  order: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
-  import: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
-  // bff: 'https://.execute-api.eu-west-1.amazonaws.com/dev'
+  product: 'https://l6mg2h5d9g.execute-api.eu-west-1.amazonaws.com/dev',
+  order: 'https://l6mg2h5d9g.execute-api.eu-west-1.amazonaws.com/dev',
+  import: 'https://l6mg2h5d9g.execute-api.eu-west-1.amazonaws.com/dev',
   bff: 'https://l6mg2h5d9g.execute-api.eu-west-1.amazonaws.com/dev'
 };
 
