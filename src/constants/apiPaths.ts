f73a99367bf996a07c9f9df478a57c35dafc0d95
Julia Ivanova <julia_sji85@mail.ru>
2020-12-06 23:30:28 +0300

    feat: implement task 8.2

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -3,7 +3,8 @@ const API_PATHS = {
   order: 'https://lkidc65ax5.execute-api.eu-west-1.amazonaws.com/dev',
   import: 'https://a7iew7bnge.execute-api.eu-west-1.amazonaws.com/dev',
   bff: 'https://lkidc65ax5.execute-api.eu-west-1.amazonaws.com/dev',
-  cart: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
+  // cart: 'http://julia-ivv-cart-api-develop.eu-west-1.elasticbeanstalk.com/api',
+  cart: 'https://tieh2x67lg.execute-api.eu-west-1.amazonaws.com/dev/api',
 };
 
 export default API_PATHS;
